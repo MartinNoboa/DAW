1d20697166fe4c6c101d984c2e7ee3176027c113
MartinNoboa <nomonoboa@gmail.com>
2020-02-23 20:03:04 -0600

    Prob 1 arreglado, prob 2-3 agregados

--- Lab4/script.js
+++ Lab4/script.js
@@ -1,3 +1,4 @@
+//-------------------------------------------------------------------------problema 1
 function recibirNum() {
     let num = prompt("Introduzca un numero: ");
     while (isNaN(num)){
@@ -9,15 +10,15 @@ function recibirNum() {
 
 function generarArreglo(n) {
     let numeros =[];
-    for(let i = 0; i < n; i++){
-        numeros[i] = i+1;
+    for(let i = 1; i <= n; i++){
+        numeros.push(i, i*i,i*i*i);
     }
     
     /*for(let i = 0; i < n; i++){
         console.log(numeros[i]);
     }
     */
-    generarTabla(numeros , n);
+    generarTabla(numeros);
 }
 
 function generarTabla(numeros, n) {
@@ -28,11 +29,95 @@ function generarTabla(numeros, n) {
         let cell = row.insertCell();
         cell.innerHTML = i;
         count++;
-        if (count%5==0) {
+        if (count%3==0) {
             row = table.insertRow();
         }
     }
     document.getElementById("tabla").appendChild(table);
 }
+
+
+
+//-------------------------------------------------------------------------problema 2
+
+function suma() {
+    let nums = [Math.round((Math.random()*20)+1) , Math.round((Math.random()*100)+1) ];
+    let tiempoInicio = new Date();
+    let respuesta = prompt("Cual es el resultado de " + nums[0] + " + " + nums[1] + "?");
+    
+    while(isNaN(respuesta)){
+        respuesta = prompt("Ingrese un valor numerico. \nCual es la suma de " + nums[0] + " + " + nums[1] + "?");
+        
+    }
+    
+    //console.log(respuesta);
+    alert(((nums[0] + nums[1] == respuesta)?"Correcta":"Incorrecta") + " en " + (new Date() - tiempoInicio)/1000 );
+}
+
+
+
+function numeros () {
+    let a = [];
+    for(let i = 0; i < 20; i++){
+        a[i] = Math.round(Math.random()*20 - 10);
+    }
+    
+    
+    revisarNumeros(a);
+}
+
+function revisarNumeros(numeros) {
+    let contador = [0,0,0];
+    
+    for (let i in numeros){
+        if(numeros[i] < 0){
+            contador[0]++;
+        }else if (numeros[i] == 0) {
+            contador[1]++;
+        }else if (numeros[i] > 0) {
+            contador[2]++;
+        }
+    }
+    
+    mostrarMensaje(contador);
+    return contador;
+}
+
+function mostrarMensaje (contador){
+    alert("Cantidad de numeros negativos = " + contador[0] + "\nCantidad de ceros = " + contador[1]+ "\nCantidad de positivos = " + contador[2]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
